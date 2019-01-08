984bd82cfbb27ec619c01da6892c898170c6ac8c
wolfpups <986328883@qq.com>
2019-01-08 11:39:59 +0800

    1-8

--- MyPro.py
+++ MyPro.py
@@ -1,66 +1,85 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import itchat,pygame,time,datetime
+import itchat,pygame,time,datetime,sys
 
-flag="THIS SHADE/SIZE: SOLD OUT" #缺货标志
+
 proFile="缺货产品URL.txt"        #需要检测的产品链接文件
 wxFile="微信备注名.txt"          #需要推送消息的微信列表
 musFile="v2.mp3"                 #提示音文件
+title=""
+
+options = Options()
+options.add_argument('--headless')# 无头模式启动
+options.add_argument('--disable-gpu')# 谷歌文档提到需要加上这个属性来规避bug
 
 def catchUrl(url):
     #捕获产品链接,返回是否有货标志
-    options = Options()
-    options.add_argument('--headless')# 无头模式启动
-    options.add_argument('--disable-gpu')# 谷歌文档提到需要加上这个属性来规避bug
+    global title
     driver= webdriver.Chrome(options=options)
-    text=flag
+    result=False
     try:
         driver.get(url)#获取页面
-        time.sleep(10)
-        text = driver.find_element_by_class_name('sold-out').text
-    except BaseException:
-        driver.quit()
-        return text
-    driver.quit()#退出关闭浏览器
-    return text
+        #time.sleep(10)
+        addButton=driver.find_element_by_class_name("product-full__add-button")
+        soldOutLi=driver.find_element_by_class_name("sold-out")
+        addDisplayed=addButton.is_displayed()
+        soldOutDisplayed=soldOutLi.is_displayed()
+        if addDisplayed and soldOutDisplayed==False:
+            result=True#有货
+        elif soldOutDisplayed and addDisplayed==False:
+            result=False#没货
+        else:
+            result=False
+        title=driver.title.split(" | ")[0]
+    except:
+        result=False
+    finally:
+        driver.quit()#退出关闭浏览器
+        return result
 
-def openBrowser(url):
-    driver= webdriver.Chrome()
-    driver.maximize_window()
-    driver.get(url)
-    driver.quit()
+
 def sendMessage(url):
     #向微信推送货物消息
     wxs=open(wxFile).readlines()#读取接受微信消息列表
-    msg=proName(url)+"有货,链接："+url
+    wxs=getList(wxFile)
+    msg=title+"有货,链接："+url
     for wx in wxs:
         user=itchat.search_friends(name=wx.strip())
         userName = user[0]['UserName']
         itchat.send(msg=msg,toUserName=userName)
 
 def playMusic():
+    #播放提示音
     pygame.mixer.init()
     track = pygame.mixer.music.load(musFile)
     pygame.mixer.music.play(-1)
 
-def proName(url):
-    return  (' '.join([word.capitalize() for word in url.split("/")[-1].split("-")])).rstrip()
+def getList(filename):
+    lines=open(filename).readlines()
+    lines=[line.strip() for line in lines if line.strip()!='']
+    return lines
 
 
-itchat.auto_login(hotReload=True)
+if getList(wxFile)==[]:
+    print("微信联系人列表为空，要用微信提醒请在"+wxFile+"中添加联系人")
+else:
+    itchat.auto_login(hotReload=True)
+    
+
 
 while True:
-    urls=open(proFile).readlines()#读取url列表
-    urls=[url.strip() for url in urls if url.strip()!='']
+    urls=getList(proFile)
     newUrls=[]#未补货的列表
     for url in urls:
-        if flag==catchUrl(url):
-            newUrls.append(url)
-            print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"  "+proName(url) + " 没货\n链接："+url)
-        else:
-            sendMessage(url)
+        if catchUrl(url):
+            if getList(wxFile)!=[]:
+                sendMessage(url)
             playMusic()
-            print(proName(url)+"有货\n链接："+url)
-    if newUrls is None:
+            print(title+"有货\n链接："+url+"\n")            
+        else:
+            newUrls.append(url)
+            print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"  "+title + " 没货\n链接："+url+"\n")
+    if newUrls==[]:
+        print("没有新的链接需要检测,添加链接后重新运行程序")
         break
     open(proFile,'w').write('\n'.join(newUrls))
